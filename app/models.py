e16e185bfeb30367073160e0490a6dbbdac75e8d
Ihimbru-k <kanihims25@gmail.com>
2025-06-06 03:42:05 +0100

    completed login, enrollment, enrollment status, upload course list and set session use cases

--- app/models.py
+++ app/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine, Column, Integer, String, LargeBinary, Float, DateTime, Boolean, ForeignKey
+from sqlalchemy import create_engine, Column, Integer, String, Float, DateTime, Boolean, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from pydantic import BaseModel
