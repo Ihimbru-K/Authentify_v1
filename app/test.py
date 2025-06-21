484bbae26eea1397e55d09821ed3a692d25d524a
Ihimbru-k <kanihims25@gmail.com>
2025-06-21 01:09:33 +0100

    fixed signup and login endpoints

--- app/test.py
+++ app/test.py
@@ -1,11 +1,5 @@
-# from app.security import get_password_hash
-# print(get_password_hash("password"))
+from app.security import get_password_hash
 
-import datetime
-import logging
-
-from requests import session
-
-
-now = datetime.utcnow()
-logging.debug(f"Current UTC time: {now}, Session start: {session.start_time}, end: {session.end_time}")
+password = "come"  
+new_hash = get_password_hash(password)
+print(new_hash)
