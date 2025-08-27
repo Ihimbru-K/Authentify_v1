bfb3bc0fe815995488e179c0b778bfe801fc847f
Ihimbru-k <kanihims25@gmail.com>
2025-08-27 09:46:34 +0100

    added procfile

--- app/main.py
+++ app/main.py
@@ -27,9 +27,6 @@ Base.metadata.create_all(bind=engine)
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
 
-
-
-
 @app.post("/auth/signup")
 async def signup(admin: AdminSignup, db: Session = Depends(get_db)):
     try:
